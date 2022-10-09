5775bd53c2eb403900021da02608315ad12ab403
millerd <millerdicarlo@gmail.com>
2022-10-09 15:57:44 -0400

    update src files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './components/Header';
+// import Footer from './components/Footer'; 
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <Header></Header>
+    </div>
+    {/* <div>
+      <Footer></Footer>
+    </div> */}
+    </>
+  );
+}
+
+export default App;

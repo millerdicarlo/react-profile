1c8751887c6dc192e8cd020f016c42a29b5573a0
millerd <millerdicarlo@gmail.com>
2022-10-09 15:47:15 -0400

    components updated

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {   
+    return (
+    <div className="page-footer" id="footer">
+        <a href=""><img src="https://img.icons8.com/ios/80/000000/github.png" alt="github-logo" class="icon"/></a>
+        <a href=""><img src="https://img.icons8.com/fluent/80/000000/linkedin-circled.png" alt="linkedin-logo" class="icon"/></a>
+        <a href=""><img src="https://img.icons8.com/ios/80/000000/stackoverflow.png" alt="stack-overflow" class="icon"/></a>
+    </div>
+    )
+}
+
+export default Footer;
